b8726bfd3f5fc417ab3590e3ab29a8377fb8f325
Alexander Grygoriev <truedevelop@yahoo.com>
2022-05-15 14:22:54 +0300

    menu burger DONE

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,20 @@
 
 document.addEventListener('DOMContentLoaded',()=>{
   
-  console.log(' JS script WORKING!');
+  const menu = document.querySelector('.header__menu'),
+  menuItem = document.querySelectorAll('.header__menu_list'),
+  hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('header__menu_active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('header__menu_active');
+      })
+  })
 
 });
